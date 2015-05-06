785681203618b0ac426617844d7dfb93feaf3f42
winfa <shenghua.lian@careerbuilder.com>
2015-05-06 22:45:30 +0800

    add server

---
+++ app/controllers/wechat_controller.rb
@@ -0,0 +1,6 @@
+class WechatController < ApplicationController
+    def server
+        array = ["wechat", params[:timestamp], params[:nonce]].sort
+        render :text => "Forbidden", :status => 403 if params[:signature] != Digest::SHA1.hexdigest(array.join)
+    end
+end
