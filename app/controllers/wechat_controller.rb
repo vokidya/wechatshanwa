13f869992a8093edc0e16f3c6c735cc8d9159b70
winfa <shenghua.lian@careerbuilder.com>
2015-05-06 23:14:10 +0800

    update

--- app/controllers/wechat_controller.rb
+++ app/controllers/wechat_controller.rb
@@ -1,6 +1,14 @@
 class WechatController < ApplicationController
     def server
-        array = ["wechat", params[:timestamp], params[:nonce]].sort
+        # array = ["wechat", params[:timestamp], params[:nonce]].sort
+        # render :text => "Forbidden", :status => 403 if params[:signature] != Digest::SHA1.hexdigest(array.join)
+        check_wechat_signature
+        render :text => params[:echostr]
+    end
+
+    private
+    def check_wechat_signature
+        array = ["weixin", params[:timestamp], params[:nonce]].sort
         render :text => "Forbidden", :status => 403 if params[:signature] != Digest::SHA1.hexdigest(array.join)
     end
 end
