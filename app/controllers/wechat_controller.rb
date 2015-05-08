cf42c1cceea1737c00994d4c582bc51314e2082f
winfa <shenghua.lian@careerbuilder.com>
2015-05-08 11:25:46 +0800

    hdjshadkhsjal

--- app/controllers/wechat_controller.rb
+++ app/controllers/wechat_controller.rb
@@ -10,24 +10,30 @@ class WechatController < ApplicationController
         #binding.pry
         #xml_file.css("//ToUserName")[0].content
         #check_wechat_signature
-        Wechatlog.create(:logkey=>"visiting",:logvalue=>DateTime.now.to_i)
-        Wechatlog.create(:logkey=>"params",:logvalue=>params.to_s)
 
-        Wechatlog.create(:logkey=>"params_xml",:logvalue=>params[:xml])
-        Wechatlog.create(:logkey=>"params_xml",:logvalue=>params["xml"])
+        # Wechatlog.create(:logkey=>"visiting",:logvalue=>DateTime.now.to_i)
+        # Wechatlog.create(:logkey=>"params",:logvalue=>params.to_s)
 
-        xml_body = request.body.read
-        Wechatlog.create(:logkey=>"request.body.read",:logvalue=>xml_body.to_s)
-        xml = Nokogiri::XML(xml_body)
-        Wechatlog.create(:logkey=>"save after xml",:logvalue=>"after xml")
-        @content = xml.css("//MediaId")[0].content
-        Wechatlog.create(:logkey=>"content",:logvalue=>@content)
+        # Wechatlog.create(:logkey=>"params_xml",:logvalue=>params[:xml])
+        # Wechatlog.create(:logkey=>"params_xml",:logvalue=>params["xml"])
 
-        @client_user = xml.css("//FromUserName")[0].content
-        Wechatlog.create(:logkey=>"content",:logvalue=>@client_user)
-        @server_user = xml.css("//ToUserName")[0].content
-        Wechatlog.create(:logkey=>"content",:logvalue=>@server_user)
-        #@render :text => result
+        # xml_body = request.body.read
+        # Wechatlog.create(:logkey=>"request.body.read",:logvalue=>xml_body.to_s)
+        # xml = Nokogiri::XML(xml_body)
+        # Wechatlog.create(:logkey=>"save after xml",:logvalue=>"after xml")
+        # @content = xml.css("//MediaId")[0].content
+        # Wechatlog.create(:logkey=>"content",:logvalue=>@content)
+
+        # @client_user = xml.css("//FromUserName")[0].content
+        # Wechatlog.create(:logkey=>"content",:logvalue=>@client_user)
+        # @server_user = xml.css("//ToUserName")[0].content
+        # Wechatlog.create(:logkey=>"content",:logvalue=>@server_user)
+
+        xml_body = params[:xml]
+        @content = xml_body["MediaId"]
+        @client_user = xml_body["FromUserName"]
+        @server_user = xml_body["ToUserName"]
+
     end
 
     def show_log
