6dda1e7d2f0242bcd513a54e797a54dfc5f3f0f9
winfa <shenghua.lian@careerbuilder.com>
2015-05-08 16:22:58 +0800

    djhada

--- app/controllers/wechat_controller.rb
+++ app/controllers/wechat_controller.rb
@@ -7,37 +7,12 @@ class WechatController < ApplicationController
     end
 
     def post_server
-        #binding.pry
-        #xml_file.css("//ToUserName")[0].content
-        #check_wechat_signature
-
-        # Wechatlog.create(:logkey=>"visiting",:logvalue=>DateTime.now.to_i)
-        # Wechatlog.create(:logkey=>"params",:logvalue=>params.to_s)
-
-        # Wechatlog.create(:logkey=>"params_xml",:logvalue=>params[:xml])
-        # Wechatlog.create(:logkey=>"params_xml",:logvalue=>params["xml"])
-
-        # xml_body = request.body.read
-        # Wechatlog.create(:logkey=>"request.body.read",:logvalue=>xml_body.to_s)
-        # xml = Nokogiri::XML(xml_body)
-        # Wechatlog.create(:logkey=>"save after xml",:logvalue=>"after xml")
-        # @content = xml.css("//MediaId")[0].content
-        # Wechatlog.create(:logkey=>"content",:logvalue=>@content)
-
-        # @client_user = xml.css("//FromUserName")[0].content
-        # Wechatlog.create(:logkey=>"content",:logvalue=>@client_user)
-        # @server_user = xml.css("//ToUserName")[0].content
-        # Wechatlog.create(:logkey=>"content",:logvalue=>@server_user)
-
         xml_body = params[:xml]
-        Wechatlog.create(:logkey=>"xml_body",:logvalue=>xml_body)
-        @content = xml_body["MediaId"]
+    
+        @media_id = xml_body["MediaId"]
         Wechatlog.create(:logkey=>"content",:logvalue=>@content)
         @client_user = xml_body["FromUserName"]
-        Wechatlog.create(:logkey=>"client_user",:logvalue=>@client_user)
         @server_user = xml_body["ToUserName"]
-        Wechatlog.create(:logkey=>"server_user",:logvalue=>@server_user)
-
     end
 
     def show_log
